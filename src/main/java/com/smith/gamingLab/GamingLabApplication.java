dc729f8e8041d2a05b0ba371d4f2c95a2e7f749e
Stefany Alicea <salicea@smith.edu>
2019-01-30 22:30:39 -0500

    configuring h2

---
+++ src/main/java/com/smith/gamingLab/GamingLabApplication.java
@@ -0,0 +1,14 @@
+package com.smith.gamingLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamingLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamingLabApplication.class, args);
+	}
+
+}
+
