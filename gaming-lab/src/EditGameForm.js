64ee91163cf4f9f48ae2eedd9f163807abac42f4
Faith Kim <fkim@smith.edu>
2019-03-29 14:12:46 -0400

    getting rid of spaces

--- gaming-lab/src/EditGameForm.js
+++ gaming-lab/src/EditGameForm.js
@@ -19,13 +19,12 @@ class EditGameForm extends Component {
     //passing a list of master games set by id
     const masterGames = this.props.value;
     const game = masterGames[0];
+
     return(
       <div>
         <form onSubmit={this.handleUpdate}>
           Title:
            <input type = "text" value={game.game.title}/>
-
-
            <input type = "submit" value = "Update" />
         </form>
         Genre:
