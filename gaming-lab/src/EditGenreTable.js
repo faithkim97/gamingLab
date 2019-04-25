e8dece3e6f97a5aa25ee45e921a14016f5bd3707
Faith Kim <fkim@smith.edu>
2019-04-25 17:22:05 -0400

    changing color

--- gaming-lab/src/EditGenreTable.js
+++ gaming-lab/src/EditGenreTable.js
@@ -63,7 +63,7 @@ class EditGenreTable extends Component {
       });
 
       return(
-        <div style={{backgroundColor: "#CDD0FF", width:"50%", padding: "2%"}}>
+        <div style={{backgroundColor: "white", width:"50%", padding: "2%"}}>
       <Table striped bordered hover variant="dark" size="sm">
         <thead>
           <tr>
