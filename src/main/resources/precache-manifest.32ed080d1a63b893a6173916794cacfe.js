7c691230ddd6f3af43aeee35be741b9dc61da707
Faith Kim <fkim@smith.edu>
2019-05-02 21:45:20 -0400

    build not working

---
+++ src/main/resources/precache-manifest.32ed080d1a63b893a6173916794cacfe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6edee20e160e504b62d2",
+    "url": "/static/js/main.1589659d.chunk.js"
+  },
+  {
+    "revision": "b607916207b3db2140eb",
+    "url": "/static/js/2.c47e96cf.chunk.js"
+  },
+  {
+    "revision": "6edee20e160e504b62d2",
+    "url": "/static/css/main.9574c143.chunk.css"
+  },
+  {
+    "revision": "7739f37b9382c6aabdf5d843a59c7663",
+    "url": "/index.html"
+  }
+];
