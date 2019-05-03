2a5ee555e3e120233be51e685cb817d707a6aa28
Faith Kim <fkim@smith.edu>
2019-05-02 21:49:44 -0400

    IT'S WORKING OMG

--- src/main/java/com/smith/gamingLab/controller/HomeController.java
+++ src/main/java/com/smith/gamingLab/controller/HomeController.java
@@ -1,15 +1,28 @@
 package com.smith.gamingLab.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
+@RequestMapping("/")
 public class HomeController {
 
+    @RequestMapping("/home")
+    @ResponseBody
+    private ClassPathResource home() {
+        return new ClassPathResource("/index.html");
+    }
 
-//    @RequestMapping("/")
-//    private String home() {
-//        return "index";
-//    }
+    @RequestMapping("/game/search")
+    @ResponseBody
+    private ClassPathResource search() {
+        return new ClassPathResource("/index.html");
+    }
+
+    
+
 }
