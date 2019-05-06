403726508b2edf5866e6ebce28cc9f482a69226e
Faith Kim <fkim@smith.edu>
2019-05-05 22:59:34 -0400

    going back to postgres to check h2db

--- src/main/java/com/smith/gamingLab/GamingLabApplication.java
+++ src/main/java/com/smith/gamingLab/GamingLabApplication.java
@@ -1,6 +1,6 @@
 package com.smith.gamingLab;
 //import org.h2.tools.Server;
-import org.h2.tools.Server;
+//import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -13,14 +13,19 @@ import java.sql.SQLException;
 @SpringBootApplication
 public class GamingLabApplication {
 	public static void main(String[] args) {
-		try {
-			Server.createTcpServer().start();
-			SpringApplication.run(GamingLabApplication.class, args);
-		} catch(SQLException e) {
-			System.exit(-1);
-
-		}
+		SpringApplication.run(GamingLabApplication.class, args);
 	}
-
 }
+//public class GamingLabApplication {
+//	public static void main(String[] args) {
+//		try {
+//			Server.createTcpServer().start();
+//			SpringApplication.run(GamingLabApplication.class, args);
+//		} catch(SQLException e) {
+//			System.exit(-1);
+//
+//		}
+//	}
+//
+//}
 
