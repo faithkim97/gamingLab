20a6b4dbe8105895da54a705f40ebbcd23682698
Faith Kim <fkim@smith.edu>
2019-05-06 22:38:51 -0400

    add game working

--- src/main/java/com/smith/gamingLab/table/Game.java
+++ src/main/java/com/smith/gamingLab/table/Game.java
@@ -60,6 +60,10 @@ public class Game {
 
     public void setId(int id) {this.id = id;}
 
+    public void setDescription(String description) {
+        this.description = description.getBytes();
+    }
+
 
 
 }
